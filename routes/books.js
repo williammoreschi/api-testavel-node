4e994c05a51e23a8cfafa0d32ee5857464126616
William Moreschi <williammoreschi@gmail.com>
2019-05-23 16:20:27 -0300

    Parte-1 Criado o local onde vai ficar as rotas da aplicação e tabém a pasta onde vai ficar os controllers...

---
+++ routes/books.js
@@ -0,0 +1,34 @@
+import BooksControlles from '../controllers/books';
+
+export default(app,Books)=>{
+app.route('/books').get((req, res) => {
+    Books.findAll({})
+        .then(result => res.json(result))
+        .catch(() => res.status(412));
+    });
+    
+    app.route('/books').post((req, res) => {
+    Books.create(req.body)
+        .then(result => res.json(result))
+        .catch(() => res.status(412));
+    });
+    
+    app.route('/books/:id').get((req, res) => {
+    Books.findOne({ where: req.params })
+        .then(result => res.json(result))
+        .catch(() => res.status(412));
+    });
+    
+    app.route('/books/:id').put((req, res) => {
+    Books.update(req.body, { where: req.params })
+        .then(result => res.json(result))
+        .catch(() => res.status(412));
+    });
+    
+    
+    app.route('/books/:id').delete((req, res) => {
+    Books.destroy({ where: req.params })
+        .then(() => res.sendStatus(204))
+        .catch(() => res.status(412));
+    });     
+}
---
+++ test/unit/controllers/books.js
@@ -0,0 +1,6 @@
+describe('Controlles Books', () => {
+    describe('Get All books: getAll()',()=>{
+        it('shoult return list of books',()=>{
+        });
+    });
+});
