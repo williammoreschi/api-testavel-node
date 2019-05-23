0890f084f37ee3bec38be764d496385a5728d2dd
William Moreschi <williammoreschi@gmail.com>
2019-05-23 13:46:46 -0300

    Criacao das rotas (insert,selectOne,update,delete) tanto no teste como na aplicaçao...

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 import express from 'express';
 import config from './config/config';
 import datasource from './config/datasource';
- 
+import bodyParser from 'body-parser';
 const app = express();
 app.config = config;
 app.set('port',7000);
 app.datasource = datasource(app);
-
+app.use(bodyParser.json());
 const Books = app.datasource.models.Books;
 
 app.route('/books').get((req,res) =>{
     Books.findAll({})
     .then(result=>res.json(result))
+    .catch(err => res.status(412));
+});
+
+app.route('/books').post((req,res) =>{
+    Books.create(req.body)
+    .then(result=>res.json(result))
+    .catch(err => res.status(412));
+});
+
+app.route('/books/:id').get((req,res) =>{
+    Books.findOne({where:req.params})
+    .then(result=>res.json(result))
+    .catch(err => res.status(412));
+});
+
+app.route('/books/:id').put((req,res) =>{
+    Books.update(req.body,{where:req.params})
+    .then(result=>res.json(result))
+    .catch(err => res.status(412));
+});
+
+
+app.route('/books/:id').delete((req,res) =>{
+    Books.destroy({where:req.params})
+    .then(result=>res.sendStatus(204))
     .catch(err => res.status(412));
 });
 
