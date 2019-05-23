0b9eeee370491dd31c1ef7c676bf39daf806ad4c
William Moreschi <williammoreschi@gmail.com>
2019-05-23 14:47:21 -0300

    Foi colocado um mondulo o Eslint e vamos usar linter do airbnb para manter um padrao de código

--- config/datasource.js
+++ config/datasource.js
@@ -3,38 +3,38 @@ import fs from 'fs';
 import path from 'path';
 
 let database = null;
-const loadModels = (sequelize) =>{
-    const dir = path.join(__dirname, '../models');
-    let models = [];
+const loadModels = (sequelize) => {
+  const dir = path.join(__dirname, '../models');
+  const models = [];
 
-    fs.readdirSync(dir).forEach(file =>{
-        const modelDir = path.join(dir,file),
-        model = sequelize.import(modelDir);
-        models[model.name] = model;
-    });
-    return models;
+  fs.readdirSync(dir).forEach((file) => {
+    const modelDir = path.join(dir, file);
+
+
+    const model = sequelize.import(modelDir);
+    models[model.name] = model;
+  });
+  return models;
 };
 export default (app) => {
-    if(!database){
-         const config = app.config;
-         const sequelize = new Sequelize(
-             config.database,
-             config.username,
-             config.password,
-             config.params
-         );
+  if (!database) {
+    const config = app.config;
+    const sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config.params,
+    );
 
-         database = {
-             sequelize,
-             Sequelize,
-             models:{}
-         };
+    database = {
+      sequelize,
+      Sequelize,
+      models: {},
+    };
 
-         database.models = loadModels(sequelize);
+    database.models = loadModels(sequelize);
 
-         sequelize.sync().done( () => {
-            return database;
-        }); 
-     }
-     return database; 
+    sequelize.sync().done(() => database);
+  }
+  return database;
 };
