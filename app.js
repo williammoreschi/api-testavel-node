935da38992f61f1883b4e6ad04f126443cc366ce
William Moreschi <williammoreschi@gmail.com>
2019-05-23 13:13:24 -0300

    Criando uma model Books. FOi colocado o sqlite pra mexer com DB e sequelize que é um orm vai ser usado pra integraçao entre banco e aplicação

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from 'express';
-
+import config from './config/config';
+import datasource from './config/datasource';
+ 
 const app = express();
+app.config = config;
+app.set('port',7000);
+app.datasource = datasource(app);
+
+const Books = app.datasource.models.Books;
+
 app.route('/books').get((req,res) =>{
-    res.json([{
-        id:1,
-        name:'Default Books'
-    }]);
+    Books.findAll({})
+    .then(result=>res.json(result))
+    .catch(err => res.status(412));
 });
 
 export default app;
--- test/integration/app.js
+++ test/integration/app.js
@@ -1,8 +1,18 @@
 describe('Routes Books', () => {
+    const Books = app.datasource.models.Books;
     const defaultBook = {
         id: 1,
         name:'Default Books'
     };
+
+    beforeEach(done => {
+        Books
+        .destroy({ where: {}})
+        .then(() => Books.create(defaultBook))
+        .then(() => {
+            done();
+        });
+    });
 
     describe('Route GET /books', () => {
         it('should return list of books', (done) => {
