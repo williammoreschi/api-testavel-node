cce33c7b017ea6425c18e2e89a1210b8405588eb
William Moreschi <williammoreschi@gmail.com>
2019-05-24 13:30:06 -0300

    Foi dado inicio a cricação do controller de usuário para acesso da api, foi colocado tbm o modulo de httpStatus e bcrypt...

---
+++ models/Users.js
@@ -0,0 +1,44 @@
+import bcrypt from 'bcrypt';
+
+export default(sequelize, DataType) => {
+    const Users = sequelize.define('Users',{
+        id:{
+            type: DataType.INTEGER,
+            primaryKey:true,
+            autoIncrement:true,
+        },
+        name:{
+            type: DataType.STRING,
+            allowNull: false,
+            validade:{
+                notEmpty:true
+            }
+        },
+        email:{
+            type: DataType.STRING,
+            allowNull: false,
+            validade:{
+                notEmpty:true
+            }
+        },
+        password:{
+            type: DataType.STRING,
+            allowNull: false,
+            validade:{
+                notEmpty:true
+            }
+        }
+    },
+    {
+        hooks:{
+            beforeCreate: user =>{
+                const salt = bcrypt.genSaltSync();
+                user.set('password',bcrypt.hashSync.password, salt);
+            }
+        },
+        classMethods:{
+            isPassword: (encodePassword, password) => bcrypt.compareSync(password,encodePassword);
+        }
+    });
+    return Users;
+}
