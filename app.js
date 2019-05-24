54492b9d60113066678d4257e11ededb50ced916
William Moreschi <williammoreschi@gmail.com>
2019-05-24 08:07:51 -0300

    Integrado o controller com as rotas

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ import bodyParser from 'body-parser';
 import config from './config/config';
 import datasource from './config/datasource';
 import booksRouter from './routes/books';
+
 const app = express();
 app.config = config;
 app.set('port', 7000);
 app.datasource = datasource(app);
 app.use(bodyParser.json());
 const Books = app.datasource.models.Books;
-booksRouter(app,Books);
-
+booksRouter(app, Books);
 
 
 export default app;
--- routes/books.js
+++ routes/books.js
@@ -1,34 +1,45 @@
 import BooksController from '../controllers/books';
 
-export default(app,Books)=>{
-app.route('/books').get((req, res) => {
-    Books.findAll({})
-        .then(result => res.json(result))
-        .catch(() => res.status(412));
-    });
-    
-    app.route('/books').post((req, res) => {
-    Books.create(req.body)
-        .then(result => res.json(result))
-        .catch(() => res.status(412));
-    });
-    
-    app.route('/books/:id').get((req, res) => {
-    Books.findOne({ where: req.params })
-        .then(result => res.json(result))
-        .catch(() => res.status(412));
-    });
-    
-    app.route('/books/:id').put((req, res) => {
-    Books.update(req.body, { where: req.params })
-        .then(result => res.json(result))
-        .catch(() => res.status(412));
-    });
-    
-    
-    app.route('/books/:id').delete((req, res) => {
-    Books.destroy({ where: req.params })
-        .then(() => res.sendStatus(204))
-        .catch(() => res.status(412));
-    });     
-}
+export default(app) => {
+  const booksController = new BooksController(app.datasource.models.Books);
+
+  app.route('/books').get((req, res) => {
+    booksController.getAll()
+      .then((response) => {
+        res.status(response.statusCode);
+        res.json(response.data);
+      });
+  });
+
+  app.route('/books').post((req, res) => {
+    booksController.create(req.body)
+      .then((response) => {
+        res.status(response.statusCode);
+        res.json(response.data);
+      });
+  });
+
+  app.route('/books/:id').get((req, res) => {
+    booksController.getById(req.params)
+      .then((response) => {
+        res.status(response.statusCode);
+        res.json(response.data);
+      });
+  });
+
+  app.route('/books/:id').put((req, res) => {
+    booksController.update(req.body, req.params)
+      .then((response) => {
+        res.status(response.statusCode);
+        res.json(response.data);
+      });
+  });
+
+
+  app.route('/books/:id').delete((req, res) => {
+    booksController.delete(req.params)
+      .then((response) => {
+        res.sendStatus(response.statusCode);
+      });
+  });
+};
